962460c45350f28baea22d055c116a792edd0cc2
B Kleinen <kleinen@infrastructure.de>
2015-04-10 15:50:11 +0200

    class 10.04.2015

---
+++ eclipse-workspace-demo/HelloWorld/src/de/infrastructure/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package de.infrastructure.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Eclipse");
+
+	}
+
+}
---
+++ eclipse-workspace-demo/HelloWorld/test/de/infrastructure/helloworld/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package de.infrastructure.helloworld;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
